516e6e7f9d57c8cf4840b9d7eb006b8b27c78363
Philip Rosen <philipjrosen@gmail.com>
2015-05-02 18:18:15 -0700

    tests popping is in LIFO order

--- stack_spec.rb
+++ stack_spec.rb
@@ -19,11 +19,25 @@ describe Stack do
   it "reports a size of 1 after adding two items and removing one" do
     @stack.push('a')
     @stack.push('b')
-    @stack.pop()
+    @stack.pop
     expect(@stack.size).to eq 1
   end
 
   it "returns nil when popping from an empty stack" do
     expect(@stack.pop).to eql nil
+  end
+
+  it "removes the most recently added of two items" do
+    @stack.push('a')
+    @stack.push('b')
+    expect(@stack.pop).to eq 'b'
+  end
+
+  it "removes the newest item, after newer items have already been added and removed" do
+    @stack.push('a')
+    @stack.push('b')
+    @stack.push('c')
+    @stack.pop
+    expect(@stack.pop).to eq 'b'
   end
 end
